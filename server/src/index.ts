b93e44cc0708f461155e5aa90af6d4ca1f6df7b9
Emilia Burza <emila.burza@gmail.com>
2025-02-22 12:52:33 +0100

    Add middleware to server index file

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,11 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config({ path: '.env' });
 
+//create the express app
 const app = express();
+
+//middleware
+app.use(express.json());
 
 //start the server
 const port = process.env.PORT || 3001;
