a66e488ae1618bfaa9b365385a60adc75df5b65c
Emilia Burza <emila.burza@gmail.com>
2025-02-23 15:57:50 +0100

    Create sidebar structure

--- client/src/components/layout/Sidebar.tsx
+++ client/src/components/layout/Sidebar.tsx
@@ -1,7 +1,38 @@
-export default function SearchBar() {
+import logoIcon from '../../assets/icons/logo.svg';
+import navHomeIcon from '../../assets/icons/icon-nav-home.svg';
+import navMoviesIcon from '../../assets/icons/icon-nav-movies.svg';
+import navTvSeriesIcon from '../../assets/icons/icon-nav-tv-series.svg';
+import navBookmarkedIcon from '../../assets/icons/icon-nav-bookmark.svg';
+import userAvatar from '../../assets/avatar/image-avatar.png';
+import { Link } from 'react-router-dom';
+
+export default function Sidebar() {
   return (
-    <div>
-      <h1>Sidebar</h1>
-    </div>
+    <section>
+      <img src={`${logoIcon}`} alt="Logo" />
+      <ul>
+        <li>
+          <Link to="/">
+            <img src={`${navHomeIcon}`} alt="Home" />
+          </Link>
+        </li>
+        <li>
+          <Link to="/movies">
+            <img src={`${navMoviesIcon}`} alt="Movies" />
+          </Link>
+        </li>
+        <li>
+          <Link to="/tv-series">
+            <img src={`${navTvSeriesIcon}`} alt="TV Series" />
+          </Link>
+        </li>
+        <li>
+          <Link to="/bookmarked">
+            <img src={`${navBookmarkedIcon}`} alt="Bookmarked" />
+          </Link>
+        </li>
+        <img src={`${userAvatar}`} alt="User avatar" />
+      </ul>
+    </section>
   );
 }
--- client/src/types/custom.d.ts
+++ client/src/types/custom.d.ts
@@ -1,8 +1,24 @@
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
+
 declare module '*.svg' {
-  import * as React from 'react';
+  const value: string;
+  export default value;
+}
 
-  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+declare module '*.jpg' {
+  const value: string;
+  export default value;
+}
 
-  export const ReactComponent;
-  export default ReactComponent;
+declare module '*.jpeg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.gif' {
+  const value: string;
+  export default value;
 }
