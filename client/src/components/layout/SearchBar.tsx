62da3374fbd5d27bac29cf2c21436a36d365bcf1
Emilia Burza <emila.burza@gmail.com>
2025-02-23 16:00:22 +0100

    Create search bar structure

--- client/src/components/layout/SearchBar.tsx
+++ client/src/components/layout/SearchBar.tsx
@@ -1,6 +1,9 @@
+import searchIcon from '../../assets/icons/icon-search.svg';
+
 export default function SearchBar() {
   return (
     <div>
+      <img src={`${searchIcon}`} alt="Search" />
       <input type="text" placeholder="Search for a movie or TV series" />
     </div>
   );
