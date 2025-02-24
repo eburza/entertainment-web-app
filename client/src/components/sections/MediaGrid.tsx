bb3ce68e87f3ae37912c19b08e63c086f96bdd8c
Emilia Burza <emila.burza@gmail.com>
2025-02-24 14:29:18 +0100

    Create base grid layout

--- client/src/components/sections/MediaGrid.tsx
+++ client/src/components/sections/MediaGrid.tsx
@@ -1,15 +1,20 @@
 import MediaCard from '../shared/MediaCard';
+import imageUrl from '../../mockData/assets/thumbnails/dogs/regular/large.jpg';
 
 export default function MediaGrid() {
   return (
     <section>
-      <h2>Media Grid</h2>
-      <MediaCard />
-      <MediaCard />
-      <MediaCard />
-      <MediaCard />
-      <MediaCard />
-      <MediaCard />
+      <h2 className="text-3xl font-light mb-8">Recommended for you</h2>
+      <div className="grid grid-cols-4 gap-8">
+        <MediaCard imageUrl={imageUrl} variant="regular" />
+        <MediaCard imageUrl={imageUrl} variant="regular" />
+        <MediaCard imageUrl={imageUrl} variant="regular" />
+        <MediaCard imageUrl={imageUrl} variant="regular" />
+        <MediaCard imageUrl={imageUrl} variant="regular" />
+        <MediaCard imageUrl={imageUrl} variant="regular" />
+        <MediaCard imageUrl={imageUrl} variant="regular" />
+        <MediaCard imageUrl={imageUrl} variant="regular" />
+      </div>
     </section>
   );
 }
