3f98f6d358eb58c3673f8f5831d9b30980de6acc
Emilia Burza <emila.burza@gmail.com>
2025-02-27 14:08:09 +0100

    Add app context provider to App router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AppProvider } from './context/appContext';
 import Layout from './components/layout/Layout';
 import Home from './pages/Home';
 import Movies from './pages/Movies';
@@ -10,16 +11,18 @@ import './styles.css';
 function App() {
   return (
     <React.StrictMode>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="/movies" element={<Movies />} />
-            <Route path="/tv-series" element={<TVSeries />} />
-            <Route path="/bookmarked" element={<Bookmarked />} />
-          </Route>
-        </Routes>
-      </Router>
+      <AppProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="/movies" element={<Movies />} />
+              <Route path="/tv-series" element={<TVSeries />} />
+              <Route path="/bookmarked" element={<Bookmarked />} />
+            </Route>
+          </Routes>
+        </Router>
+      </AppProvider>
     </React.StrictMode>
   );
 }
