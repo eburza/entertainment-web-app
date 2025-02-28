43f796b65037a9e64fec94fe9212c33491723f65
Emilia Burza <emila.burza@gmail.com>
2025-02-28 16:29:07 +0100

    Update bookmark button

--- client/src/components/shared/BookmarkButton.tsx
+++ client/src/components/shared/BookmarkButton.tsx
@@ -1,10 +1,12 @@
 import bookmarkIconOn from '../../assets/icons/icon-bookmark-full.svg';
 import bookmarkIconOff from '../../assets/icons/icon-bookmark-empty.svg';
-
 import { useState } from 'react';
+import { IBookmarkButtonProps } from '../../types/interface';
 
-export default function BookmarkButton() {
-  const [isBookmarked, setIsBookmarked] = useState(false);
+export default function BookmarkButton({
+  isBookmarked: initialIsBookmarked = false,
+}: IBookmarkButtonProps) {
+  const [isBookmarked, setIsBookmarked] = useState(initialIsBookmarked);
 
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
@@ -12,8 +14,11 @@ export default function BookmarkButton() {
   };
 
   return (
-    <button onClick={handleClick}>
-      <img src={`${isBookmarked ? bookmarkIconOn : bookmarkIconOff}`} alt="Bookmark icon" />
+    <button
+      onClick={handleClick}
+      className="w-8 h-8 bg-black/50 rounded-full flex items-center justify-center"
+    >
+      <img src={isBookmarked ? bookmarkIconOn : bookmarkIconOff} alt="Bookmark icon" />
     </button>
   );
 }
