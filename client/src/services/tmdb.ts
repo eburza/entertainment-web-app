a0f3ddae6c4150098e17dcb4241ee1c089fb3cfa
Emilia Burza <emila.burza@gmail.com>
2025-03-03 14:13:49 +0100

    Add api endpoint to get all shows

--- client/src/services/tmdb.ts
+++ client/src/services/tmdb.ts
@@ -13,6 +13,37 @@ const options = {
   },
 };
 
+//get all shows
+export async function getAllShows() {
+  try {
+    const [moviesResponse, tvResponse] = await Promise.all([
+      fetch(
+        `${TMDB_BASE_URL}/discover/movie?include_adult=false&language=en-US&page=1&sort_by=popularity.desc`,
+        options
+      ),
+      fetch(
+        `${TMDB_BASE_URL}/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=popularity.desc`,
+        options
+      ),
+    ]);
+
+    if (!moviesResponse || !tvResponse) {
+      throw new Error('Failed to fetch shows');
+    }
+    const [moviesData, tvData] = await Promise.all([moviesResponse.json(), tvResponse.json()]);
+
+    const moviesReceived = moviesData.results;
+    const tvReceived = tvData.results;
+
+    const allShows = [...moviesReceived, ...tvReceived].sort(() => Math.random() - 0.5);
+
+    return allShows;
+  } catch (error) {
+    console.error('Error fetching shows:', error);
+    throw error;
+  }
+}
+
 // get all trending
 export async function getAllTrending() {
   try {
