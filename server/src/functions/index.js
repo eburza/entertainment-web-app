8102bc25870dd8d40dc54b5dc6db1eb542438cbd
Emilia Burza <emila.burza@gmail.com>
2025-03-04 19:33:14 +0100

    Remove netlify functions

--- server/src/functions/index.js
+++
@@ -1,9 +0,0 @@
-// The actual serverless function is in api.ts to be used with netlify functions (ensures the functions directory is properly created
-module.exports = {
-  handler: async () => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Server is running' })
-    };
-  }
-}; 
