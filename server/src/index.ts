ce5ff4e3e0c07fc1512afce9dc2eb0192949af9c
Emilia Burza <emila.burza@gmail.com>
2025-03-04 15:46:35 +0100

    Update index routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,18 @@
+import dotenv from 'dotenv';
+import path from 'path';
+// Load environment variable
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
 import showsRouter from './routes/shows';
 import bookmarkedRouter from './routes/bookmarked';
+import tvRouter from './routes/tv';
+import moviesRouter from './routes/movies';
+import searchRouter from './routes/search';
 import connectToDatabase from './config/db';
 import errorHandler from './middleware/error';
 import logging from './config/logger';
 import express from 'express';
-import dotenv from 'dotenv';
 import cors from 'cors';
-dotenv.config({ path: '.env' });
 
 //connect to the database
 connectToDatabase();
@@ -24,9 +30,11 @@ app.use(cors());
 app.use(express.json());
 
 //routes
-app.use('/shows', showsRouter);
+app.use('/', showsRouter);
 app.use('/bookmarked', bookmarkedRouter);
-
+app.use('/tv', tvRouter);
+app.use('/movies', moviesRouter);
+app.use('/search', searchRouter);
 
 //error handling middleware
 app.use(errorHandler);
