2e68cadf0b7d77aea3333e2c0d59479441ce4254
Emilia Burza <emila.burza@gmail.com>
2025-03-08 16:16:02 +0100

    Hide search bar on login page

--- client/src/components/layout/Layout.tsx
+++ client/src/components/layout/Layout.tsx
@@ -1,13 +1,16 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import SearchBar from './SearchBar';
 import Sidebar from './Sidebar';
 
 export default function Layout() {
+  const location = useLocation();
+  const isAuthPage = location.pathname === '/login' || location.pathname === '/register';
+
   return (
     <div className="flex flex-col md:flex-row min-h-screen bg-dark w-full">
       <Sidebar />
       <main className="flex-1 px-4 md:pl-32 md:pr-8 pt-8">
-        <SearchBar />
+        {!isAuthPage && <SearchBar />}
         <Outlet />
       </main>
     </div>
