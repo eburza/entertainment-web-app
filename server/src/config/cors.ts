799ee2c383d5595267616a3eb6fe1d5f3296f477
Emilia Burza <emila.burza@gmail.com>
2025-03-11 14:10:07 +0100

    Configure CORS

---
+++ server/src/config/cors.ts
@@ -0,0 +1,10 @@
+import cors from 'cors';
+
+// Configure CORS
+export const corsMiddleware = cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+  credentials: true,
+  maxAge: 86400
+});
